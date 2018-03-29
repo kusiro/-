feab484cd30bf5cc170c5fa0fb5abb777ab37483
kusiro <jackysoon871008@gmail.com>
2018-03-30 01:11:52 +0800

    Add contact

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import home_style from './css/home.scss';
 import team_style from './css/team.scss';
 import equipment_style from './css/equipment.scss';
 import promote_style from './css/promote.scss';
+import contact_style from './css/contact.scss';
 import tech_style from './css/tech.scss';
 import html from '../index.html';
 import Router from './router.js';
